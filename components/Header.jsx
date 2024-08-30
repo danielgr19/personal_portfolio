aa91c9f992197db4138f0c41fd8f08fc47ddc407
danielgr19 <dani.grra19@gmail.com>
2024-08-30 14:20:02 -0500

    Se modifican colores, ademas de mejorar componente Header y Nav

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,29 @@
 import Link from "next/link";
-import { button } from "./ui/button"
+import { Button } from "./ui/button"
+
+//Components
+import Nav from "./Nav";
 
 const Header = () => {
    return <header className="py-8 xl:py-12 text-white bg-pink-50/20"> 
-        <div className="container mx-auto">
+        <div className="container mx-auto flex justify-between items-center">
             {/* LOGO */}
             <Link href="/">
-                <h1 className="text-4xl font-semibold">Daniel <span className="text-accent">.</span></h1>
+                <h1 className="text-4xl font-semibold">
+                <span className="text-accent"> &lt; </span> Daniel Gr <span className="text-accent"> &gt; </span></h1>
             </Link>
 
-            {/* Desktop Nav */}
+            {/* Desktop Nav & hire me button */}
+            <div className="hidden xl:flex items-center gap-8">
+                <Nav />
+                <Link href="/contact">
+                    <Button> Hire me </Button>
+                </Link>
+            </div>
             
+            <div className="xl:hidden">mobile nav</div>
         </div>
    </header>;
 };
 
 export default Header
- 
